7b7f0a345aee443c6e8284b8e427ef7bccc6996a
melijov <53196304+melijov@users.noreply.github.com>
2020-02-07 13:42:20 -0500

    fixed bugs

--- csv_pandas.py
+++ csv_pandas.py
@@ -8,13 +8,13 @@ def iter_df(file_name, time_cols):
   yield from pd.read_csv(file_name,parse_dates=time_cols, chunksize=100)
   
 def print_file(file_name, time_cols):
-  print(load_df(fn,time_cols).head())
-  for i, df in enumerate(iter_df(fn, tc)):
+  print(load_df(file_name,time_cols).head())
+  for i, df in enumerate(iter_df(file_name, time_cols)):
     if i > 10:
       break
     print(len(df))  
 
-def user_test():
+def unit_test():
   fn = 'taxi.csv.bz2'
   tc =['tpep_dropoff_datetime', 'tpep_pickup_datetime']
   print_file(fn, tc)
